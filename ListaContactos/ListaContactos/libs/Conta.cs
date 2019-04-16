623f904f2603ec28aae0c3b8117a90f32a09a730
Mikas-Atec <miguel.pereira.t0114411@edu.atec.pt>
2019-04-16 23:25:40 +0100

    comeco listacontactos

---
+++ ListaContactos/ListaContactos/libs/Conta.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaContactos
+{
+    public class Conta
+    {
+        private string _user;
+        private string _nome;
+        
+        public Conta(string user,string nome)
+        {
+            User = user;
+            Nome = nome;
+        }
+
+        public string User
+        {
+            get { return _user; }
+            set { if (validarString(value)) _user = value; }
+        }
+        public string Nome
+        {
+            get { return _nome; }
+            set { if (validarString(value)) _nome = value; }
+        }
+
+        private bool validarString(string s)
+        {
+            return !String.IsNullOrEmpty(s) && !String.IsNullOrWhiteSpace(s) && s.Count()<100;
+        }
+    }
+}
---
+++ ListaContactos/ListaContactos/libs/DAL.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaContactos
+{
+    internal class DAL
+    {
+        private OleDbConnection _conexao;
+        private string _tabela;
+
+        public DAL(string tabela) {
+            _conexao = new OleDbConnection();
+            _conexao.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=db\\listacontactos.mdb;";
+            _tabela = tabela;
+        }
+
+        public OleDbDataReader find(string s)
+        {
+            if (_conexao.State != System.Data.ConnectionState.Open)
+                _conexao.Open();
+
+
+            OleDbCommand comando = new OleDbCommand();
+            comando.CommandText = "SELECT "+s+ " FROM [" + _tabela + "]";
+            
+            comando.Connection = _conexao;
+            return comando.ExecuteReader();
+        }
+
+        public OleDbDataReader find(string s,string a)
+        {
+            if (_conexao.State != System.Data.ConnectionState.Open)
+                _conexao.Open();
+
+
+            OleDbCommand comando = new OleDbCommand();
+            comando.CommandText = "SELECT " + s + " FROM ["+_tabela+"] "+a;
+
+            comando.Connection = _conexao;
+            return comando.ExecuteReader();
+        }
+
+
+    }
+}
